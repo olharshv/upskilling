9660a15f2dac32f58bd5124c5cf6321fe2beac3b
Olga <1q2w3e$r>
2018-09-13 16:04:45 +0300

    tests for homework

--- src/test/java/com/luxoft/dbdc/upskilling/implementations/TodoListItemImplTest.java
+++ src/test/java/com/luxoft/dbdc/upskilling/implementations/TodoListItemImplTest.java
@@ -25,6 +25,48 @@ class TodoListItemImplTest {
         Assertions.assertTrue(title.equalsIgnoreCase(todoItem.getTitle()));
     }
 
+//    @Test
+//    public void toogle_an_item() {
+//        item.toggle();
+//        Assertions.assertTrue(item.isToggled());
+//    }
+//
+//    @Test
+//    public void untoggle_an_item() {
+//        item.toggle();
+//        item.toggle();
+//        Assertions.assertFalse(item.isToggled());
+//    }
+//
+ //   @Test
+ //  public void item_is_not_toggled_after_creation() {
+//       Assertions.assertFalse(item.isToggled());
+//    }
+//
+//    @Test
+//    public void item_have_title() {
+//        Assertions.assertEquals("Todo item for test", item.getTitle());
+//    }
+//
+//    @Test
+//    public void cannot_create_item_with_empty_title() {
+//        Assertions.assertThrows(IllegalArgumentException.class,
+//                () -> new TodoListItemImpl(""));
+//    }
+//
+//    @Test
+//    public void can_set_new_title() {
+//        String new_title = "New title for test item";
+//        TodoListItemImpl newItem = item.setTitle(new_title);
+//        Assertions.assertEquals(new_title, newItem.getTitle());
+//    }
+//
+//    @Test
+//    public void cannot_set_empty_title() {
+//        Assertions.assertThrows(IllegalArgumentException.class,
+//                () -> item.setTitle(""));
+//    }
+
     @AfterEach
     public void tearDown() {
 
